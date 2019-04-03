7726d2a8013ff7d5bd6967a485eacb341af89765
panzerstadt <tliqun@gmail.com>
2019-04-03 17:18:05 +0900

    Updates

---
+++ precache-manifest.a883abd294d8320118b01da6f8412064.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "34a4fb2271e61a8cf1f412f566bef34e",
+    "url": "/danceoff/static/media/video.34a4fb22.mp4"
+  },
+  {
+    "revision": "21dce757ce4772631e909fc97814a801",
+    "url": "/danceoff/static/media/loop.21dce757.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/danceoff/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f298f1ebc624671c1eadab26c38bef5",
+    "url": "/danceoff/static/media/danceoff.8f298f1e.png"
+  },
+  {
+    "revision": "fdcd4bddabb27ab6c5ce",
+    "url": "/danceoff/static/js/runtime~main.6509b7b9.js"
+  },
+  {
+    "revision": "53f67080ff1f5762389e",
+    "url": "/danceoff/static/js/main.d187a0e0.chunk.js"
+  },
+  {
+    "revision": "ee31a4bdd6a80db7bad8",
+    "url": "/danceoff/static/js/2.ce36ed8c.chunk.js"
+  },
+  {
+    "revision": "53f67080ff1f5762389e",
+    "url": "/danceoff/static/css/main.e0f735b7.chunk.css"
+  },
+  {
+    "revision": "c3e9e2aab9d15771bad36923ae934067",
+    "url": "/danceoff/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/danceoff/precache-manifest.da2c9e89ed3c5a9927025669af4ccb04.js"
+  "/danceoff/precache-manifest.a883abd294d8320118b01da6f8412064.js"
 );
 
 workbox.clientsClaim();
