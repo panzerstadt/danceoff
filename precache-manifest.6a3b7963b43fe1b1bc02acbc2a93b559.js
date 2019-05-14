0d9b88870205c1be156e4743f3e10f662782d11f
panzerstadt <tliqun@gmail.com>
2019-05-14 13:56:14 +0900

    Updates

---
+++ precache-manifest.6a3b7963b43fe1b1bc02acbc2a93b559.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "21dce757ce4772631e909fc97814a801",
+    "url": "/danceoff/static/media/loop.21dce757.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/danceoff/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "8f298f1ebc624671c1eadab26c38bef5",
+    "url": "/danceoff/static/media/danceoff.8f298f1e.png"
+  },
+  {
+    "revision": "caad1c74d26c98c96f7fa85b832eef03",
+    "url": "/danceoff/static/media/baseball.caad1c74.jpg"
+  },
+  {
+    "revision": "6c3859ba493bf9e550334bd52e70c2bc",
+    "url": "/danceoff/static/media/BASEBALL.6c3859ba.mp4"
+  },
+  {
+    "revision": "fdcd4bddabb27ab6c5ce",
+    "url": "/danceoff/static/js/runtime~main.6509b7b9.js"
+  },
+  {
+    "revision": "218d091aec3a59ec2d2b",
+    "url": "/danceoff/static/js/main.3fb58ed4.chunk.js"
+  },
+  {
+    "revision": "9d0acb1c96b931e0e28b",
+    "url": "/danceoff/static/js/2.9702885d.chunk.js"
+  },
+  {
+    "revision": "218d091aec3a59ec2d2b",
+    "url": "/danceoff/static/css/main.7d884956.chunk.css"
+  },
+  {
+    "revision": "e4a27ff04fe1fd257f17a056f8bf309d",
+    "url": "/danceoff/index.html"
+  }
+];
